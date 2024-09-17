fe18cb1c53946cde04c77ee60bd688e54be359d8
aparadeveloper <aparajita.subramanyam@twooceansstrategy.com>
2024-09-17 15:34:35 +0530

    Design changes for login and register pages

--- x-saf-dashboard/src/views/Login.vue
+++ x-saf-dashboard/src/views/Login.vue
@@ -1,37 +1,45 @@
 <template>
-<section class="bg-gray-50 dark:bg-gray-900">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#" @submit.prevent="handleSubmit">
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                      <input type="text" name="email" id="email" v-model="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com">
-                      <p id="email_error" ref="email_error" class="text-rose-400">{{email_error}}</p>
+<section class="bg-[url('/public/background.png')] bg-cover bg-center">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-[480px] h-[646px] bg-white rounded-[8px] p-[48px] space-y-[32px] shadow">
+            <div class="w-[179.26px] h-[48px]">
+                <div class="flex gap-2">
+                    <img class="" src="/public/images/Vector.svg" alt="image description">
+                    <img class="" src="/public/images/Group.svg" alt="image description">
+                </div>
+            </div>
+          <div class="w-[384px] h-[320px] space-y-[20px] font-Manrope font-medium">
+              <div class="text-[32px] text-left text-[#0F0F0F]">
+                  Log in to your account
+              </div>
+              <form class="w-[384px] h-[198px] space-y-[24px] text-left" action="#" @submit.prevent="handleSubmit">
+                  <div class="w-[384px] h-[87px] space-y-[12px]">
+                      <label for="email" class="text-[#0F0F0F] text-[16px]">Email</label>
+                      <input type="text" name="email" id="email" v-model="email" class="bg-[#F8F9FA] text-[#74797C] rounded-[4px] focus:bg-white focus:border-[#DDDDDD] focus:ring-0 focus:ring-[#DDDDDD] border border-[#DDDDDD] w-[384px] h-[56px] p-[16px]" placeholder="">
+                      <span id="email_error" ref="email_error" class="text-rose-400">{{email_error}}</span>
                   </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" v-model="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                      <p id="password_error" ref="password_error" class="text-rose-400">{{password_error}}</p>
+                  <div class="w-[384px] h-[87px] space-y-[12px]">
+                      <label for="password" class="text-[#0F0F0F] text-[16px]">Password</label>
+                      <input type="password" name="password" id="password" v-model="password" placeholder="" class="bg-[#F8F9FA] text-[#74797C] rounded-[4px] focus:bg-white focus:border-[#DDDDDD] focus:ring-0 focus:ring-[#DDDDDD] border border-[#DDDDDD] w-[384px] h-[56px] p-[16px]">
+                      <span id="password_error" ref="password_error" class="text-rose-400">{{password_error}}</span>
                   </div>
-                  <div class="flex items-center justify-between">
+                  <div class="flex items-center justify-between w-[384px] h-[20px] pt-[25px]">
                       <div class="flex items-start">
                           <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
+                            <input v-model="rememberMe" id="rememberMe" aria-describedby="remember" type="checkbox" class="w-[15px] h-[15px] border border-[#74797C] focus:ring-0">
                           </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                          <div class="ml-3">
+                            <label for="remember" class="text-[#74797C] text-[16px] ">Remember me</label>
                           </div>
                       </div>
-                      <router-link to="" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</router-link>
+                      <router-link to="" class="text-[16px] font-medium text-[#12B87C] hover:underline">Forgot password?</router-link>
                   </div>
-                  <button type="submit" class="w-full text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <router-link to="/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</router-link>
-                  </p>
+                  <div class="w-[384px] h-[100px] space-y-[16px] text-center pt-[30px]">
+                    <button type="submit" class="w-[384px] h-[56px] text-white bg-[#12B87C] rounded-[4px] px-[24px] py-[16px] text-center">Log in</button>
+                    <p class="text-[16px] text-[#74797C] pt-[10px]">
+                        Don’t have an account yet? <router-link to="/register" class="text-[#12B87C] hover:underline">Sign up</router-link>
+                    </p>
+                  </div>
               </form>
           </div>
       </div>
@@ -64,11 +72,28 @@ export default{
             email: '',
             password: '',
             email_error:'',
-            password_error:''
+            password_error:'',
+            rememberMe: false
+        }
+    },
+    mounted() {
+        // Check if a email is saved in localStorage and autofill the field
+        const savedemail = localStorage.getItem('email');
+        if (savedemail) {
+            this.email = savedemail;
+            this.rememberMe = true;
         }
     },
     methods: {
         async handleSubmit() {
+            // Handle your login logic here
+            if (this.rememberMe) {
+                // Save the email in localStorage
+                localStorage.setItem('email', this.email);
+            } else {
+                // Clear the email from localStorage if not "Remember Me"
+                localStorage.removeItem('email');
+            }
             try{
                 let token = localStorage.getItem('token');
                 const response = await axios.post('login', 
--- x-saf-dashboard/src/views/Register.vue
+++ x-saf-dashboard/src/views/Register.vue
@@ -1,47 +1,55 @@
 <template>
-<section class="bg-gray-50 dark:bg-gray-900">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Register your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#" @submit.prevent="handleSubmit">
-                  <div>
-                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                      <input type="name" name="name" id="name" v-model="name" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name">
-                      <p id="name_error" ref="name_error" class="text-rose-400">{{name_error}}</p>
+<section class="bg-[url('/public/background.png')] bg-cover bg-center">
+  <div class="flex flex-col items-center justify-center p-[50px] font-medium font-Manrope text-left mx-auto md:h-screen lg:py-0">
+      <div class="w-[820px] h-[719px] bg-white rounded-[8px] shadow p-[48px]">
+          <div class="w-[179.26px] h-[48px]">
+                <div class="flex gap-2">
+                    <img class="" src="/public/images/Vector.svg" alt="image description">
+                    <img class="" src="/public/images/Group.svg" alt="image description">
+                </div>
+          </div>
+          <div class="p-[48px] w-[624px] h-[379px] space-y-[32px]">
+              <div class="text-[32px] text-[#0F0F0F]">
+                  Create your X-SAF account
+              </div>
+              <form class="w-[624px] h-[309px] space-y-[24px]" action="#" @submit.prevent="handleSubmit">
+                    <div class="w-[624px] h-[87px] flex gap-[24px]">
+                        <div class="flex flex-col w-[300px] h-[87px]">
+                            <label for="name" class="text-[16px] text-[#0F0F0F] pb-[12px]">Name</label>
+                            <input type="name" name="name" id="name" v-model="name" class="w-[300px] h-[56px] bg-[#F8F9FA] text-[#74797C] text-[16px] border border-[#DDDDDD] rounded-[4px] focus:bg-white focus:ring-0 focus:ring-[#DDDDDD] focus:border-[#DDDDDD] p-[16px]" placeholder="Enter your Name">
+                            <span id="name_error" ref="name_error" class="text-rose-400 text-center">{{name_error}}</span>
+                        </div>
+                        <div class="flex flex-col w-[300px] h-[87px]">
+                            <label for="email" class="text-[16px] text-[#0F0F0F] pb-[12px]">Email</label>
+                            <input type="text" name="email" id="email" v-model="email" class="w-[300px] h-[56px] bg-[#F8F9FA] text-[#74797C] text-[16px] border border-[#DDDDDD] rounded-[4px] focus:bg-white focus:ring-0 focus:ring-[#DDDDDD] focus:border-[#DDDDDD] p-[16px]" placeholder="Enter your email">
+                            <p id="email_error" ref="email_error" class="text-rose-400 text-center">{{email_error}}</p>
+                        </div>
                   </div>
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                      <input type="text" name="email" id="email" v-model="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com">
-                      <p id="email_error" ref="email_error" class="text-rose-400">{{email_error}}</p>
+                  <div class="w-[624px] h-[87px] flex gap-[24px]">
+                        <div class="flex flex-col w-[300px] h-[87px]">
+                            <label for="password" class="text-[16px] text-[#0F0F0F] pb-[12px]">Password</label>
+                            <input type="password" name="password" id="password" v-model="password" placeholder="Enter your password" class="w-[300px] h-[56px] bg-[#F8F9FA] text-[#74797C] text-[16px] border border-[#DDDDDD] rounded-[4px] focus:bg-white focus:ring-0 focus:ring-[#DDDDDD] focus:border-[#DDDDDD] p-[16px]">
+                            <p id="password_error" ref="password_error" class="text-rose-400 text-center">{{password_error}}</p>
+                        </div>
+                        <div class="flex flex-col w-[300px] h-[87px]">
+                            <label for="confirmPassword" class="text-[16px] text-[#0F0F0F] pb-[12px]">Confirm Password</label>
+                            <input type="password" name="confirmPassword" id="confirmPassword" v-model="confirmPassword" placeholder="Re-enter your password" class="w-[300px] h-[56px] bg-[#F8F9FA] text-[#74797C] text-[16px] border border-[#DDDDDD] rounded-[4px] focus:bg-white focus:ring-0 focus:ring-[#DDDDDD] focus:border-[#DDDDDD] p-[16px]">
+                        </div>
+                   </div>
+                  <div class="w-[624px] h-[87px] flex gap-[24px]">
+                        <div class="flex flex-col w-[300px] h-[87px]">
+                            <label for="user_type_id" class="text-[16px] text-[#0F0F0F] pb-[12px]">User type</label>
+                            <select id="user_type_id" ref="user_type_id" v-model="user_type_id" class="w-[300px] h-[56px] bg-[#F8F9FA] text-[#74797C] text-[16px] border border-[#DDDDDD] rounded-[4px] focus:bg-white focus:ring-0 focus:ring-[#DDDDDD] focus:border-[#DDDDDD] p-[16px]">
+                                    <option selected>Select</option>
+                                    <option v-for="u_type in user_type" :value="u_type.user_type_id" :key="u_type.user_type_id">{{u_type.name}}</option>
+                            </select>
+                            <p id="user_type_id_error" ref="user_type_id_error" class="text-rose-400 text-center">{{user_type_id_error}}</p>
+                        </div>
                   </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" v-model="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                      <p id="password_error" ref="password_error" class="text-rose-400">{{password_error}}</p>
+                  <div class="flex flex-col items-center justify-between pt-[48px]">
+                        <button type="submit" class="w-[624px] h-[56px] text-white bg-[#12B87C] focus:ring-0 focus:outline-none rounded-[4px] text-[16px] px-[24px] py-[16px] text-center">Sign up</button>
+                        <p class="text-[16px] font-medium text-[#0F0F0F] pt-[20px]">Have an account? <router-link to="/" class="text-[16px] text-[#12B87C] hover:underline">Sign in</router-link> </p>      
                   </div>
-                   <div>
-                      <label for="confirmPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                      <input type="password" name="confirmPassword" id="confirmPassword" v-model="confirmPassword" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                      <p id="confirmPassword_error" ref="confirmPassword_error" class="text-rose-400">{{confirmPassword_error}}</p>
-                  </div>
-                  <div>
-                  <label for="user_type_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User type</label>
-                  <select id="user_type_id" ref="user_type_id" v-model="user_type_id" class="bg-[#F8F9FA] border border-[#DDDDDD] text-[#74797C] text-[16px] rounded-[4px] block w-[300px] h-[56px] p-[16px] hover:bg-white focus:ring-[#DDDDDD] focus:ring-0 focus:border-[#DDDDDD] active:bg-white focus:bg-white">
-                        <option selected>Select</option>
-                        <option v-for="u_type in user_type" :value="u_type.user_type_id" :key="u_type.user_type_id">{{u_type.name}}</option>
-                  </select>
-                <p id="user_type_id_error" ref="user_type_id_error" class="text-rose-400">{{user_type_id_error}}</p>
-                  </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-center">
-                      </div>
-                      <router-link to="/" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</router-link>
-                  </div>
-                  <button type="submit" class="w-full text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Sign up</button>
-                  
               </form>
           </div>
       </div>
@@ -74,19 +82,12 @@ export default{
             name_error:'',
             email_error:'',
             password_error:'',
-            confirmPassword_error:'',
             user_type_id_error: ''
         }
     },
     methods: {
         async handleSubmit(){
             try{
-                if(this.user_type_id == 'Select' || this.user_type_id== ''){
-                    this.user_type_id_error = "Select a user type";
-                }
-                else{
-                    this.user_type_id_error ="";
-                }
                 let token = localStorage.getItem('token');
                 const response = await axios.post('register', 
                     {
@@ -103,27 +104,27 @@ export default{
                 });
                 this.$router.push('/');
             }
-            catch (error) {
-                if(error.response.data.errors.name){
-                    this.name_error = error.response.data.errors.name[0];
+            catch (error) { console.log(error.response.data.data);
+                if(error.response.data.data.name){
+                    this.name_error = error.response.data.data.name[0];
                 }
                 else{
                     this.name_error = "";
                 }
-                if(error.response.data.errors.email){
-                    this.email_error = error.response.data.errors.email[0];
+                if(error.response.data.data.email){
+                    this.email_error = error.response.data.data.email[0];
                 }
                 else{
                     this.email_error = "";
                 }
-                if(error.response.data.errors.password){
-                    this.password_error = error.response.data.errors.password[0];
+                if(error.response.data.data.password){
+                    this.password_error = error.response.data.data.password[0];
                 }
                 else{
                     this.password_error = "";
                 }
-                if(this.user_type_id == 'Select' || this.user_type_id== ''){
-                    this.user_type_id_error = "Select a user type";
+                if(error.response.data.data.user_type_id || this.user_type_id == 'Select' || this.user_type_id== ''){
+                    this.user_type_id_error = error.response.data.data.user_type_id[0];
                 }
                 else{
                     this.user_type_id_error ="";
